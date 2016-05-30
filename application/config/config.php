a4330d7d246e77525c28508bece1c483391f620d
Thony Hermawan <thony.caktoy@gmail.com>
2016-05-30 09:22:46 +0700

    Config

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/penilaian_restoran/';
+$config['base_url'] = 'http://localhost:8080/penilaian_restoran/';
 
 /*
 |--------------------------------------------------------------------------
